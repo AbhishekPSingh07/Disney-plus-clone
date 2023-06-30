f5720b8a17f1a3cebf3fd2300c7955175e39f6be
AbhishekPSingh07 <79076050+AbhishekPSingh07@users.noreply.github.com>
2023-06-30 11:39:23 +0530

    Add: viewers

--- src/components/ImgSlider.js
+++ src/components/ImgSlider.js
@@ -41,19 +41,29 @@ const Carousel = styled(Slider)`
         color:white;
       }
     }
-    li.slick-active button {
+    li.slick-active button::before {
       color: white;
     }
     .slick-list{
         overflow:visible;
     }
+    button{
+      z-index:1;
+    }
 `
 const Wrap = styled.div`
+    cursor:pointer;
     img{
+        border:4px solid transparent;
         border-radius:10px;
         width:100%;
         height:100%;
         box-shadow: rgb(0 0 0 /69%) 0px 26px 30px -10px,
         rgb(0 0 0 /73%) 0px 16px 10px -10px;
+        transition:300ms;
+
+        &:hover{
+          border:4px solid rgba(249, 249, 249,0.8);
+        }
     }
 `
